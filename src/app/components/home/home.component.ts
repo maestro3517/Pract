406e7312e3c099c52e4c72207ffc607fc59d2df6
maestro3517 <49096955+maestro3517@users.noreply.github.com>
2020-01-29 18:25:24 +0530

    lists

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  lists: Array<any>;
   constructor() { }
 
   ngOnInit() {
+    this.lists = Array(5).fill(null);
   }
+
 
 }
